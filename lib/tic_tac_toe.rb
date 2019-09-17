89d1f6b29bbe98b0bf722f27b8ed79398ec02a23
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-09-17 03:49:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,5 +53,16 @@ class TicTacToe
   
   def turn
     puts "Please enter a move 1-9:"
+    input = gets.chomp
+    index = input_to_index(input)
+      if valid_move?(index)
+
+        player = current_player
+        move(index, player)
+        display_board
+      else
+        turn
+      end
+    end
   end
 end
