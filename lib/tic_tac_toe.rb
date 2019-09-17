1930b084c16900c14c3e94a35ca7af23cb29de2b
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-09-17 03:09:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,7 +35,7 @@ class TicTacToe
     @board[index] == "X" || @board[index] == "O"
   end
   
-  def valid_move?(position)
+  def valid_move?(index
     if @board[index].position_taken?
       false
     else 
